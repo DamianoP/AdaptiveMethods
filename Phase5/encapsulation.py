e45bbc2c428081493688953ff92931fa45fd93d5
DamianoP <damy06@hotmail.it>
2019-01-05 17:35:47 +0100

    Update encapsulation.py

--- Phase5/encapsulation.py
+++ Phase5/encapsulation.py
@@ -5,7 +5,7 @@ folders=os.listdir(".")
 
 processes = []
 for i in range(0,len(folders)):
-	p=subprocess.Popen(["python","e.py",folders[i],"default"])
+	p=subprocess.Popen(["python","script.py",folders[i],"default"])
 	processes.append(p)
 
 for p in processes:
@@ -13,7 +13,7 @@ for p in processes:
 
 processes = []
 for i in range(0,len(folders)):
-	p=subprocess.Popen(["python","e.py",folders[i],"uint8"])
+	p=subprocess.Popen(["python","script.py",folders[i],"uint8"])
 	processes.append(p)
 
 for p in processes:
